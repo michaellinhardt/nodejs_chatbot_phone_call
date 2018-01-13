b85914c8aa9351026d16679d0cf750ad6ce77f44
Michael Linhardt <michael.linhardt@recast.ai>
2018-01-13 22:50:05 +0100

    feat(nexmo): link config

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
 import _log from './log'
 import _sequelize from './sequelize'
 import _express from './express'
+import _nexmo from './nexmo'
 
 module.exports = {
 	_log,
 	_sequelize,
 	_express,
+	_nexmo,
 }
