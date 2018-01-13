a4fdbf2131471ade88d0e05be648c95fdcefce7e
Michael Linhardt <michael.linhardt@recast.ai>
2018-01-13 06:34:28 +0100

    feat(doc): comment class and methods

--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@ import {
 	SequelizeController,
 } from './controller'
 
+/*
+** Class Dolores
+** This is the Bot Server entry point (also know as app.js)
+** This is a class who's call at the end of the file
+** This class wake up everything to make the bot ready
+**
+*/
+
 class Dolores extends ExtendController {
 	constructor () { super({ name: __filename }) }
 
+	/*
+	** Method start()
+	** - init_controller():
+	**   -  load all controller constructor method in the right place
+	*/
 	start () {
 		const function_name = 'start()'
 		try {
@@ -18,6 +31,14 @@ class Dolores extends ExtendController {
 			process.stdout.write(`${_log.color.filename}${this.path_to_index(__filename)}${_log.color.warn}: ${function_name}\r\n${_log.color.error}${error}\r\n${_log.color.clear}`)
 		}
 	}
+
+	/*
+	** Method init_controller()
+	** load all controller constructor
+	**
+	** this.log is controlling the log ouput (file and terminal)
+	** this.sequelize is controlling the database interaction
+	*/
 
 	init_controllers () {
 		const function_name = 'init_controller()'
