cc77deb80e09c51b764ea22c33e7b0188ad500d8
Michael Linhardt <michael.linhardt@recast.ai>
2018-01-13 21:23:30 +0100

    feat(controller): link the socket controller

--- src/controller/index.js
+++ src/controller/index.js
@@ -3,6 +3,7 @@ import SequelizeController from './sequelize.controller'
 import LogController from './log.controller'
 import ExpressController from './express.controller'
 import NexmoController from './nexmo.controller'
+import SocketController from './socket.controller'
 
 module.exports = {
 	ExtendController,
@@ -10,4 +11,5 @@ module.exports = {
 	LogController,
 	ExpressController,
 	NexmoController,
+	SocketController,
 }
