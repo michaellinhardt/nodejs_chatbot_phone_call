9d323ffeef69d513d090d8169acf07972cd8e164
Michael Linhardt <michael.linhardt@recast.ai>
2018-01-13 22:48:39 +0100

    feat(nexmo): send ncco config when ping /nexmo/ncco

--- src/controller/nexmo.controller.js
+++ src/controller/nexmo.controller.js
@@ -22,7 +22,7 @@ module.exports = class SequelizeController extends ExtendController {
 			global.info(__filename, function_name, 'nexmo send data to /nexmo/events')
 
 		} catch (error) {
-			global.err(__filename, function_name, error)
+			global.err(__filename, function_name, error.stack)
 		}
 	}
 
@@ -34,40 +34,12 @@ module.exports = class SequelizeController extends ExtendController {
 	post_ncco (request, response) {
 		const function_name = 'post_ncco()'
 		try {
-			// response.writeHead(200, { 'Content-Type': 'application/json' });
-			// response.end(_nexmo, 'utf-8');
+			response.writeHead(200, { 'Content-Type': 'application/json' });
+			response.end(JSON.stringify(_nexmo), 'utf-8');
 			global.info(__filename, function_name, 'nexmo send data to /nexmo/ncco')
 
-
-			// ORIGINAL
-			fs.readFile('../config/ncco.json', function(error, data) {
-			   response.writeHead(200, { 'Content-Type': 'application/json' });
-			   response.end(data, 'utf-8');
-			   console.log("ncco.json requested");
-			});
-
-
-			// LUC
-			// console.log(req.query.from);
-			// try {
-			//   stream.client = { };
-			//   if (req.url.split('&uuid=')[1]) {
-			//     stream.client.uuid = req.url.split('&uuid=')[1];
-			//   }
-			//   stream.client.phoneNumber = req.query.from;
-			// } catch(err) {
-			//   console.log(err);
-			// }
-			// fs.readFile('./config/ncco.json', function(error, data) {
-			//   res.writeHead(200, { 'Content-Type': 'application/json' });
-			//   res.end(data, 'utf-8');
-			//   console.log("ncco.json requested");
-			// });
-
-
-
 		} catch (error) {
-			global.err(__filename, function_name, error)
+			global.err(__filename, function_name, error.stack)
 		}
 	}
 
